c8def1658a8bcb5d5165011e040dfa17e774ed3c
John Tyrone Pagunsan Coronel <jtcoronel.chmsu@gmail.com>
2026-02-10 06:34:27 +0800

    added class based views in views.py

--- api_app/views.py
+++ api_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Item, Location
+from .serializers import ItemSerializer, LocationSerializer
 
-# Create your views here.
+class ItemList(generics.ListCreateAPIView):
+    serializer_class = ItemSerializer
+    
+    def get_queryset(self):
+        queryset = Item.objects.all()
+        location = self.request.query_params.get('location')
+        if location is not None:
+            queryset = queryset.filter(itemLocation=location)
+class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ItemSerializer
+    queryset = Item.objects.all()
+    
+class LocationList(generics.ListCreateAPIView):
+    serializer_class = LocationSerializer
+    queryset = Location.objects.all()
